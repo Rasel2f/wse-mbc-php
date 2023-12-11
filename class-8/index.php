c8d9a06ac4edc78f7f68ed2504349a49633757f5
Rasel Hossain <30306452+Rasel2f@users.noreply.github.com>
2023-12-11 15:35:37 +0600

    class 8 update

--- class-8/index.php
+++ class-8/index.php
@@ -14,29 +14,72 @@
         textarea:focus, input:focus{
             outline: none;
         }
+        .output li {
+                border: 1px solid #ddd;
+                margin: 5px 0;
+                padding: 8px;
+            }
     </style>
 </head>
 <body class="bg-gray-100 p-6">
-    <div class="container">
+    <div class="container mx-auto">
         <div class="max-w-md mx-auto bg-white p-5 border rounded-md shadow-md">
-            <h1 class="text-xl font-bold underline mb-4">Welcome to my new dom project. - Class-8</h1>
+            <h1 class="text-xl font-bold underline">Form Output</h1>
+            <?php
+                $name = '';
+                $gender = '';
+                $subscribe = '';
+                $date = '';
+                $time = '';
+                $options = [];
+                $district = '';
+                $multicheckbox = [];
+                
+                if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                    $name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
+                    $gender = isset($_POST['gender']) ? htmlspecialchars($_POST['gender']) : '';
+                    $subscribe = isset($_POST['subscribe']) ? "Yes" : 'No';
+                    $date = isset($_POST['date']) ? htmlspecialchars($_POST['date']) : '';
+                    $time = isset($_POST['time']) ? htmlspecialchars($_POST['time']) : '';
+                    $options = isset($_POST['options']) ? $_POST['options'] : [];
+                    $district = isset($_POST['district']) ? $_POST['district'] : '';
+                    $multicheckbox = isset($_POST['multicheckbox']) ? $_POST['multicheckbox'] : [];
+                    
+                    echo '<div class="output"><ul>';
+                        echo '<li><strong>Name is : </strong>'. $name .'</li>';
+                        echo '<li><strong>Gender : </strong>'. $gender .'</li>';
+                        echo '<li><strong>Subscribe : </strong>'. $subscribe .'</li>';
+                        echo '<li><strong>Date is : </strong>'. $date .'</li>';
+                        echo '<li><strong>Time is : </strong>'. $time .'</li>';
+                        echo '<li><strong>Options : </strong>'. implode(", ", $options) .'</li>';
+                        echo '<li><strong>District is : </strong>'. $district .'</li>';
+                        echo '<li><strong>Fruits : </strong>'. implode(", ", $multicheckbox) .'</li>';
+                    echo '</ul></div>';
+                }
+            
+            ?>
+
+        </div>
+
+        <div class="max-w-md mx-auto bg-white p-5 border rounded-md shadow-md">
+            <h1 class="text-xl font-bold underline mb-4">Welcome to my new dom project - Class-8</h1>
 
             <form action="" method="POST">
                 <!--  -->
                 <div class="mb-4">
                     <label class="block text-sm" for="name">Name: </label>
-                    <input type="text" id="name" class="border mt-1 p-2 w-full rounded" name="name">
+                    <input value="<?php echo $name; ?>" type="text" name="name" id="name" class="border mt-1 p-2 w-full rounded" >
                 </div>
                 <!--  -->
                 <div class="mb-4">
-                    <label class="block text-sm" for="name">Gender:</label>
+                    <label class="block text-sm" for="gender">Gender:</label>
                     <div class="bt-1 spece-x-4">
                         <label for="male" class="inline-flex items-center mr-2">
-                            <input type="radio" name="gender" id="male" value="male" class="form-radio">
+                            <input <?php echo $gender === "Male" ? "checked" : ''; ?> type="radio" name="gender" value="Male" id="male" class="form-radio">
                             <span class="ml-2">Male</span>
                         </label>
                         <label for="female" class="inline-flex items-center">
-                            <input type="radio" name="gender" id="female" value="male" class="form-radio">
+                            <input <?php echo $gender === "Female" ? "checked" : ''; ?> type="radio" name="gender" value="Female" id="female" class="form-radio">
                             <span class="ml-2">Female</span>
                         </label>
                     </div>
@@ -44,56 +87,80 @@
                 <!--  -->
                 <div class="mb-4">
                     <label class="block text-sm" for="subscribe">Subscribe: </label>
-                    <input type="checkbox" id="subscribe" class="form-checkbox" name="name">
+                    <input <?php echo $subscribe === "Yes" ? "checked" : ''; ?> type="checkbox" name="subscribe" id="subscribe" class="form-checkbox">
                 </div>
                 <!--  -->
                 <div class="mb-4">
                     <label class="block text-sm" for="datepicker">Pick a date: </label>
-                    <input type="text" id="datepicker" class="border mt-1 p-2 w-full rounded" name="name">
+                    <input value="<?php echo $date; ?>" type="text" name="date" id="datepicker" class="border mt-1 p-2 w-full rounded">
+                </div>
+                <!--  -->
+                <div class="mb-4">
+                    <label class="block text-sm" for="timepicker">Pick a Time: </label>
+                    <input value="<?php echo $time; ?>" type="text" name="time" id="timepicker" class="border mt-1 p-2 w-full rounded">
                 </div>
                 <!--  -->
                 <div class="mb-4">
                     <label class="block text-sm" for="options">Select Options: </label>
-                    <select class="border mt-1 p-2 w-full rounded" name="options[]" id="options" multiple>
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-                        <option value="option3">Option 3</option>
-                        <option value="option4">Option 4</option>
-                        <option value="option5">Option 5</option>
+                    <select name="options[]" id="options" multiple class="border mt-1 p-2 w-full rounded">
+                        <option <?php echo in_array('Option1', $options) ? "selected" : ''; ?> value="Option1">Option 1</option>
+                        <option <?php echo in_array('Option2', $options) ? "selected" : ''; ?> value="Option2">Option 2</option>
+                        <option <?php echo in_array('Option3', $options) ? "selected" : ''; ?> value="Option3">Option 3</option>
+                        <option <?php echo in_array('Option4', $options) ? "selected" : ''; ?> value="Option4">Option 4</option>
+                        <option <?php echo in_array('Option5', $options) ? "selected" : ''; ?> value="Option5">Option 5</option>
                     </select>
                 </div>
                 <!--  -->
                 <div class="mb-4">
                     <label class="block text-sm" for="district">Select a District: </label>
-                    <select class="border mt-1 p-2 w-full rounded" name="district" id="district">
-                        <option value="select">Select</option>
-                        <option value="bagerhat">Bagerhat</option>
-                        <option value="khulna">Khulna</option>
-                        <option value="dhaka">Dhaka</option>
+                    <select name="district" id="district" class="border mt-1 p-2 w-full rounded">
+                        <option <?php echo $district === "Select" ? "selected" : ''; ?> value="Select">Select</option>
+                        <option <?php echo $district === "Bagerhat" ? "selected" : ''; ?> value="Bagerhat">Bagerhat</option>
+                        <option <?php echo $district === "Khulna" ? "selected" : ''; ?> value="Khulna">Khulna</option>
+                        <option <?php echo $district === "Dhaka" ? "selected" : ''; ?> value="Dhaka">Dhaka</option>
                     </select>
+                </div>
+                <!-- Multi-checkbox -->
+                <div class="mb-4">
+                    <label class="block text-sm">Select your favorite fruits</label>
+                    <div class="space-y-2">
+                        <label class="inline-flex items-center">
+                            <input <?php echo in_array('Apple', $multicheckbox) ? "checked" : ''; ?>  type="checkbox" name="multicheckbox[]" value="Apple" class="form-checkbox text-indigo-600">
+                            <span class="m-2">Apple</span>
+                        </label>
+                        <label class="inline-flex items-center">
+                            <input <?php echo in_array('Banana', $multicheckbox) ? "checked" : ''; ?> type="checkbox" name="multicheckbox[]" value="Banana" class="form-checkbox text-indigo-600">
+                            <span class="m-2">Banana</span>
+                        </label>
+                        <label class="inline-flex items-center">
+                            <input <?php echo in_array('Mango', $multicheckbox) ? "checked" : ''; ?> type="checkbox" name="multicheckbox[]" value="Mango" class="form-checkbox text-indigo-600">
+                            <span class="m-2">Mango</span>
+                        </label>
+                    </div>
                 </div>
                 <!--  -->
                 <div class="mb-4">
-                    <button type="submit" id="submit" class="border mt-1 p-2 rounded" name="submit">Submit</button>
+                    <button type="submit" class="bg-gray-600 text-white border mt-1 p-2 rounded">Submit</button>
                 </div>
 
             </form>
 
         </div>
-        <div class="max-w-md mx-auto bg-white p-5 border rounded-md shadow-md">
-            <h1 class="text-xl font-bold underline">Form Output</h1>
-        </div>
     </div>
 
     <script>
-
-        // In your Javascript (external .js resource or <script> tag)
         $(document).ready(function() {
             $('#options').select2();
         });
 
         flatpickr("#datepicker", {
+            dateFormat: "Y-m-d"
+        });
 
+        flatpickr("#timepicker", {
+            enableTime: true,
+            noCalendar: true,
+            daetFormat: "H:i"
         });
     </script>
 </body>
